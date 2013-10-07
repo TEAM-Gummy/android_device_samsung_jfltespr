5c8ed31472cbe829c415f8cc2deef6a267ac9522
Justin Rogers <justin.a.rogers@gmail.com>
2013-10-06 20:27:15 -0500

    Gummy Fied

--- tg.mk
+++ tg.mk
@@ -5,13 +5,13 @@ PRODUCT_COPY_FILES += \
     device/samsung/jfltespr/apns-conf-cdma.xml:system/etc/apns-conf.xml
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common TG stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDL release-keys"
 
 
-PRODUCT_NAME := cm_jfltespr
+PRODUCT_NAME := tg_jfltespr
 PRODUCT_DEVICE := jfltespr
